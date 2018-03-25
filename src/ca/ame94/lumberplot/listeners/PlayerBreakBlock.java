f7ed8bf4f46e483e2a133b8ca6e07c28e6d2105d
ame94 <adria.elmswood@gmail.com>
2018-03-25 00:03:12 -0300

    finished writing block break behavior for saplings to automatically re-appear after base of tree is chopped down

--- src/ca/ame94/lumberplot/listeners/PlayerBreakBlock.java
+++ src/ca/ame94/lumberplot/listeners/PlayerBreakBlock.java
@@ -1,6 +1,7 @@
 package ca.ame94.lumberplot.listeners;
 
 import ca.ame94.lumberplot.LumberPlot;
+import ca.ame94.lumberplot.util.PluginMgr;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -13,6 +14,13 @@ import org.bukkit.event.block.BlockBreakEvent;
 
 public class PlayerBreakBlock implements Listener {
 
+
+
+    /**
+     * Handle block break events. Allow only leaf and log blocks to be broken if
+     * the player doesn't have creative or the required permission node.
+     * @param event The BlockBreakEvent
+     */
     @EventHandler
     public void onPlayerBreakBlock(BlockBreakEvent event) {
         Block block = event.getBlock();
@@ -29,11 +37,28 @@ public class PlayerBreakBlock implements Listener {
                         // specifically deny changes to saplings, dirt and grass
                         event.setCancelled(true);
                         break;
-                    case LEAVES:
-                    case LEAVES_2:
                     case LOG:
                     case LOG_2:
-                        // leaves & logs are permitted
+                        Material matBelow = block.getRelative(BlockFace.DOWN).getType();
+                        if (matBelow == Material.DIRT || matBelow == Material.GRASS) {
+
+                            //todo: Don't rely on this
+                            byte data = block.getData();
+                            if (block.getType() == Material.LOG_2) {
+                                data -= 4;
+                            }
+                            byte finalData = data;
+                            PluginMgr.getPlugin().getServer().getScheduler().scheduleSyncDelayedTask(PluginMgr.getPlugin(), new Runnable() {
+                                public void run() {
+                                    block.setType(Material.SAPLING);
+                                    block.setData(finalData);
+                                }
+                            }, 1L);
+                        }
+                    case VINE:
+                    case LEAVES:
+                    case LEAVES_2:
+                        // vines, leaves & logs are permitted
                         break;
                     default:
                         // deny everything else
