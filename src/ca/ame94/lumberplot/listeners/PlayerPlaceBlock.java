e5170a333155dc0c49250086054e107e8bbc30ac
ame94 <adria.elmswood@gmail.com>
2018-03-25 00:01:35 -0300

    added block place event for inside of plots

--- src/ca/ame94/lumberplot/listeners/PlayerPlaceBlock.java
+++ src/ca/ame94/lumberplot/listeners/PlayerPlaceBlock.java
@@ -1,5 +1,9 @@
 package ca.ame94.lumberplot.listeners;
 
+import ca.ame94.lumberplot.LumberPlot;
+import org.bukkit.GameMode;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -8,7 +12,14 @@ public class PlayerPlaceBlock implements Listener {
 
     @EventHandler
     public void onPlayerPlaceBlock(BlockPlaceEvent event) {
-        // next to add
+        Block block = event.getBlock();
+        if (LumberPlot.isLocationUnderProtection(block.getLocation())) {
+            Player player = event.getPlayer();
+            boolean inCreative = player.getGameMode().equals(GameMode.CREATIVE);
+            boolean hasPermission = player.hasPermission("lumberplot.modify");
+            if (!(inCreative || hasPermission)) {
+                event.setCancelled(true);
+            }
+        }
     }
-
 }
